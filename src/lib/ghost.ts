dc8afb6f803dc4ee84a9c4c60b45b0d92150636b
Yunsup Sim <pedogunu@gmail.com>
2023-09-04 00:30:16 +0900

    Add all

--- src/lib/ghost.ts
+++ src/lib/ghost.ts
@@ -1,14 +1,7 @@
 import GhostContentAPI from '@tryghost/content-api';
-// @ts-ignore
-import GhostAdminAPI from '@tryghost/admin-api';
 // Create API instance with site credentials
 export const ghostClient = new GhostContentAPI({
     url: import.meta.env.GHOST_SITE_URL, // This is the default URL if your site is running on a local environment
     key: import.meta.env.CONTENT_API_KEY,
-    version: 'v5.0',
-});
-export const ghostAdminClient = new GhostAdminAPI({
-    url: import.meta.env.GHOST_SITE_URL, // This is the default URL if your site is running on a local environment
-    key: import.meta.env.ADMIN_API_KEY,
     version: 'v5.0',
 });
