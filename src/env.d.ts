9665b50a2c96465ac1697f869288985f0d42fe6c
Yunsup Sim <pedogunu@gmail.com>
2024-04-01 00:50:43 +0900

    오류 수정

--- src/env.d.ts
+++ src/env.d.ts
@@ -10,7 +10,6 @@ type R2Object = import('@cloudflare/workers-types').R2Object;
 type ENV = {
     SERVER_URL: string;
     KV_BINDING: KVNamespace;
-    IMAGE_R2: R2Object;
 };
 
 type Runtime = import('@astrojs/cloudflare').DirectoryRuntime<ENV>;
