56f456080e965ed50e25057dde8097ef4ed7aa5d
Yunsup Sim <pedogunu@gmail.com>
2024-04-07 20:32:46 +0900

    Add about

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,17 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
-
+import mdx from '@astrojs/mdx';
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ones-to-watch.ethansup.net',
-  integrations: [sitemap(), tailwind()],
+  integrations: [
+    sitemap(),
+    tailwind(),
+    mdx(),
+  ],
   trailingSlash: 'never',
   vite: {
     resolve: {
