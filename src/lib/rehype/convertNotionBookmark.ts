43e4c9683ba39b495f7731fdbdf9ab2f08001a97
Yunsup Sim <pedogunu@gmail.com>
2025-03-13 01:02:47 +0900

    delete trim

--- src/lib/rehype/convertNotionBookmark.ts
+++ src/lib/rehype/convertNotionBookmark.ts
@@ -36,8 +36,8 @@ export default function () {
         } satisfies ElementContent;
         const text = await r.text();
         const match = text.match(TITLE_REGEX);
-        if (match?.[1] && match[1].trim() === "") {
-          child.value = match[1].trim() + " 🔗";
+        if (match?.[1]) {
+          child.value = match[1] + " 🔗";
           v.node.children.push(child);
           return;
         }
